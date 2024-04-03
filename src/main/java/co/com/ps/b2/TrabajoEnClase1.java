45bf1f6d37525f4751b842201a46923ecfe18adc
ALejandro Mina <jamm0617@hotmail.com>
2024-04-03 18:30:48 -0500

    Trabajo en clase

---
+++ src/main/java/co/com/ps/b2/TrabajoEnClase1.java
@@ -0,0 +1,26 @@
+package co.com.ps.b2;
+//Fan
+public class TrabajoEnClase1 {
+
+  int blade;
+  String color;
+  String brand;
+  int speeds;
+  int bottons;
+  float size;
+  String engine;
+
+  public void cool(){
+    System.out.println("the fan is cooling");
+  }
+
+  public static void turn (){
+    System.out.println("turning around");
+  }
+
+  public static void dissipate(){
+  System.out.println("dissipate the heat");
+  }
+
+}
+
